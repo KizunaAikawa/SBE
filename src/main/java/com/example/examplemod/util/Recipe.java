93f143b483d47feb9f96b4dbc5741a1b849b3949
KizunaAikawa <463489334@qq.com>
2020-06-07 19:52:21 +0800

    rewrite match() and getCraftingResult()function of Recipe

--- src/main/java/com/example/examplemod/util/Recipe.java
+++ src/main/java/com/example/examplemod/util/Recipe.java
@@ -13,40 +13,50 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class Recipe extends ShapedOreRecipe {
-    private ItemStack requiredStateBlade;
+    private Map<ItemStack, RecipePos> requiredStatefulBlades;
+    private ItemStack mainBlade;
 
-    public Recipe(ResourceLocation group, ItemStack result, ItemStack requiredStateBlade, Object... recipe) {
+    private int tagValueCompare(TagPropertyAccessor<Integer> targetTag, NBTTagCompound reqTag, NBTTagCompound srcTag) {
+        return targetTag.get(reqTag).compareTo(targetTag.get(srcTag));
+    }
+
+    private boolean isValidBlade(ItemStack item) {
+        return !item.isEmpty() && item.getItem() instanceof ItemSlashBlade && item.hasTagCompound();
+    }
+
+    public Recipe(ResourceLocation group, ItemStack result, ItemStack mainBlade, Map<ItemStack, RecipePos> requiredStatefulBlades, Object... recipe) {
         super(group, result, recipe);
-        this.requiredStateBlade = ItemStack.EMPTY;
-        this.requiredStateBlade = requiredStateBlade;
+        this.mainBlade = mainBlade;
+        this.requiredStatefulBlades = requiredStatefulBlades;
     }
 
     public boolean matches(InventoryCrafting inv, World world) {
         boolean result = super.matches(inv, world);
-        if (result && !this.requiredStateBlade.isEmpty()) {
-            this.requiredStateBlade.setItemDamage(32767);
-            for (int x = 0; x < inv.getSizeInventory(); x++) {
-                ItemStack blade = inv.getStackInSlot(x);
-                if (!blade.isEmpty() && blade.getItem() instanceof ItemSlashBlade && blade.hasTagCompound()) {
-                    //附魔等级检查
-                    Set<Map.Entry<Enchantment, Integer>> oldEnchantments = EnchantmentHelper.getEnchantments(this.requiredStateBlade).entrySet();
+        if (result && !this.requiredStatefulBlades.isEmpty()) {
+            requiredStatefulBlades.forEach((blade, pos) -> blade.setItemDamage(Short.MAX_VALUE));
+            Set<ItemStack> reqBlades = requiredStatefulBlades.keySet();
+            for (ItemStack reqBlade : reqBlades) {
+                RecipePos pos = requiredStatefulBlades.get(reqBlade);
+                ItemStack srcBlade = inv.getStackInRowAndColumn(pos.getX(), pos.getY());
+                if (isValidBlade(srcBlade)) {
+                    //附魔检查
+                    Set<Map.Entry<Enchantment, Integer>> oldEnchantments = EnchantmentHelper.getEnchantments(reqBlade).entrySet();
                     for (Map.Entry<Enchantment, Integer> enchantmentEffect : oldEnchantments) {
-                        int level = EnchantmentHelper.getEnchantmentLevel(enchantmentEffect.getKey(), blade);
+                        int level = EnchantmentHelper.getEnchantmentLevel(enchantmentEffect.getKey(), srcBlade);
                         if (level < enchantmentEffect.getValue()) {
                             return false;
                         }
                     }
                     //NBTTags检查
-                    if (!blade.getUnlocalizedName().equals(this.requiredStateBlade.getUnlocalizedName())) {
+                    if (!srcBlade.getUnlocalizedName().equals(reqBlade.getUnlocalizedName())) {
                         return false;
                     }
-                    NBTTagCompound reqTag = ItemSlashBlade.getItemTagCompound(this.requiredStateBlade);
-                    NBTTagCompound srcTag = ItemSlashBlade.getItemTagCompound(blade);
+                    NBTTagCompound reqTag = ItemSlashBlade.getItemTagCompound(reqBlade);
+                    NBTTagCompound srcTag = ItemSlashBlade.getItemTagCompound(srcBlade);
                     if (0 < this.tagValueCompare(ItemSlashBlade.ProudSoul, reqTag, srcTag)) {
                         return false;
                     }
@@ -65,67 +75,61 @@ public class Recipe extends ShapedOreRecipe {
 
     public ItemStack getCraftingResult(InventoryCrafting var1) {
         ItemStack newBlade = super.getCraftingResult(var1);
-        for (int x = 0; x < var1.getSizeInventory(); x++) {
-            ItemStack srcBlade = var1.getStackInSlot(x);
-            if (!srcBlade.isEmpty() && srcBlade.getItem() instanceof ItemSlashBlade && srcBlade.hasTagCompound()) {
-                NBTTagCompound oldTag = srcBlade.getTagCompound();
-                oldTag = oldTag.copy();
-                NBTTagCompound newTag = ItemSlashBlade.getItemTagCompound(newBlade);
-                if (ItemSlashBladeNamed.CurrentItemName.exists(newTag)) {
-                    String key = ItemSlashBladeNamed.CurrentItemName.get(newTag);
-                    ItemStack tmp = SlashBlade.getCustomBlade(key);
-                    if (!tmp.isEmpty()) {
-                        newBlade = tmp;
-                    }
+        RecipePos mainBladePos = requiredStatefulBlades.get(mainBlade);
+        ItemStack srcBlade = var1.getStackInRowAndColumn(mainBladePos.getX(), mainBladePos.getY());
+        if (isValidBlade(srcBlade)) {
+            NBTTagCompound oldTag = srcBlade.getTagCompound();
+            oldTag = oldTag.copy();
+            NBTTagCompound newTag = ItemSlashBlade.getItemTagCompound(newBlade);
+            if (ItemSlashBladeNamed.CurrentItemName.exists(newTag)) {
+                String key = ItemSlashBladeNamed.CurrentItemName.get(newTag);
+                ItemStack tmp = SlashBlade.getCustomBlade(key);
+                if (!tmp.isEmpty()) {
+                    newBlade = tmp;
                 }
-                //还原附魔
-                Map<Enchantment, Integer> newItemEnchants = EnchantmentHelper.getEnchantments(newBlade);
-                Map<Enchantment, Integer> oldItemEnchants = EnchantmentHelper.getEnchantments(srcBlade);
-                Iterator var9 = oldItemEnchants.keySet().iterator();
-                Set<Map.Entry<Enchantment, Integer>> oldEnchantments = EnchantmentHelper.getEnchantments(srcBlade).entrySet();
-                for (Map.Entry<Enchantment, Integer> enchantmentEffect : oldEnchantments) {
-                    Enchantment enchantment = enchantmentEffect.getKey();
-                    int destLevel = newItemEnchants.containsKey(enchantment) ? enchantmentEffect.getValue() : 0;
-                    int srcLevel = oldItemEnchants.get(enchantment);
-                    srcLevel = Math.max(srcLevel, destLevel);
-                    srcLevel = Math.min(srcLevel, enchantment.getMaxLevel());
-                    boolean isApplicable = enchantment.canApply(newBlade);
+            }
+            //还原附魔
+            Map<Enchantment, Integer> newItemEnchants = EnchantmentHelper.getEnchantments(newBlade);
+            Map<Enchantment, Integer> oldItemEnchants = EnchantmentHelper.getEnchantments(srcBlade);
+            Set<Map.Entry<Enchantment, Integer>> oldEnchantments = EnchantmentHelper.getEnchantments(srcBlade).entrySet();
+            for (Map.Entry<Enchantment, Integer> enchantmentEffect : oldEnchantments) {
+                Enchantment enchantment = enchantmentEffect.getKey();
+                int destLevel = newItemEnchants.containsKey(enchantment) ? enchantmentEffect.getValue() : 0;
+                int srcLevel = oldItemEnchants.get(enchantment);
+                srcLevel = Math.max(srcLevel, destLevel);
+                srcLevel = Math.min(srcLevel, enchantment.getMaxLevel());
+                boolean isApplicable = enchantment.canApply(newBlade);
+                if (isApplicable) {
+                    for (Enchantment newBladeEnchantment : newItemEnchants.keySet()) {
+                        if (newBladeEnchantment != enchantment && !enchantment.isCompatibleWith(newBladeEnchantment)) {
+                            isApplicable = false;
+                            break;
+                        }
+                    }
                     if (isApplicable) {
-                        for (Enchantment newBladeEnchantment : newItemEnchants.keySet()) {
-                            if (newBladeEnchantment != enchantment && !enchantment.isCompatibleWith(newBladeEnchantment)) {
-                                isApplicable = false;
-                                break;
-                            }
-                        }
-                        if (isApplicable) {
-                            newItemEnchants.put(enchantment, srcLevel);
-                        }
+                        newItemEnchants.put(enchantment, srcLevel);
                     }
                 }
-                EnchantmentHelper.setEnchantments(newItemEnchants, newBlade);
-                //还原NBTTags
-                newTag = ItemSlashBlade.getItemTagCompound(newBlade);
-                ItemSlashBlade.KillCount.set(newTag, ItemSlashBlade.KillCount.get(oldTag));
-                ItemSlashBlade.ProudSoul.set(newTag, ItemSlashBlade.ProudSoul.get(oldTag));
-                ItemSlashBlade.RepairCount.set(newTag, ItemSlashBlade.RepairCount.get(oldTag));
-                if (oldTag.hasUniqueId("Owner")) {
-                    newTag.setUniqueId("Owner", oldTag.getUniqueId("Owner"));
-                }
-                if (oldTag.hasKey("adjustX")) {
-                    newTag.setFloat("adjustX", oldTag.getFloat("adjustX"));
-                }
-                if (oldTag.hasKey("adjustY")) {
-                    newTag.setFloat("adjustY", oldTag.getFloat("adjustY"));
-                }
-                if (oldTag.hasKey("adjustZ")) {
-                    newTag.setFloat("adjustZ", oldTag.getFloat("adjustZ"));
-                }
+            }
+            EnchantmentHelper.setEnchantments(newItemEnchants, newBlade);
+            //还原NBTTags
+            newTag = ItemSlashBlade.getItemTagCompound(newBlade);
+            ItemSlashBlade.KillCount.set(newTag, ItemSlashBlade.KillCount.get(oldTag));
+            ItemSlashBlade.ProudSoul.set(newTag, ItemSlashBlade.ProudSoul.get(oldTag));
+            ItemSlashBlade.RepairCount.set(newTag, ItemSlashBlade.RepairCount.get(oldTag));
+            if (oldTag.hasUniqueId("Owner")) {
+                newTag.setUniqueId("Owner", oldTag.getUniqueId("Owner"));
+            }
+            if (oldTag.hasKey("adjustX")) {
+                newTag.setFloat("adjustX", oldTag.getFloat("adjustX"));
+            }
+            if (oldTag.hasKey("adjustY")) {
+                newTag.setFloat("adjustY", oldTag.getFloat("adjustY"));
+            }
+            if (oldTag.hasKey("adjustZ")) {
+                newTag.setFloat("adjustZ", oldTag.getFloat("adjustZ"));
             }
         }
         return newBlade;
-    }
-
-    private int tagValueCompare(TagPropertyAccessor<Integer> targetTag, NBTTagCompound reqTag, NBTTagCompound srcTag) {
-        return targetTag.get(reqTag).compareTo(targetTag.get(srcTag));
     }
 }
---
+++ src/main/java/com/example/examplemod/util/RecipePos.java
@@ -0,0 +1,30 @@
+package com.example.examplemod.util;
+
+public class RecipePos {
+    private int posX = 0;
+    private int posY = 0;
+
+    public RecipePos(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public RecipePos() {
+    }
+
+    public int getX() {
+        return posX;
+    }
+
+    public void setX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getY() {
+        return posY;
+    }
+
+    public void setY(int posY) {
+        this.posY = posY;
+    }
+}
